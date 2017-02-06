02fd8c62a42d01bcca3ec7d4fc5d953978e09b25
alla bondarchuk <allabnd@yahoo.com>
2017-02-05 20:04:01 -0800

    complete madlib

--- JumpStart/madlibs.rb
+++ JumpStart/madlibs.rb
@@ -10,17 +10,29 @@ user_input = gets.chomp
 
 puts "Fill in the following blanks to complete our story:"
 
-puts "Pick a noun"
-noun = gets.chomp
-puts "Pick an adjective"
+puts "Pick a noun:"
+noun = gets.chomp.capitalize!
+puts "Pick an adjective:"
 adj = gets.chomp
-puts "Pick an adverb"
+puts "Pick a living thing!:"
+living_thing = gets.chomp
+puts "Pick an adverb:"
 adverb = gets.chomp
-puts "Pick a number (numerical value)"
+puts "Pick a verb:"
+verb = gets.chomp
+puts "Pick a number:"
 number = gets.chomp
-puts "Pick a verb"
-verb.upcase = gets.chomp
-puts "Pick another noun"
-noun_2 = gets.chomp
-puts "Pick a past-tense verb"
+puts "Pick a another verb:"
+verb_two = gets.chomp.upcase
+puts "Pick another noun(plural):"
+noun_two = gets.chomp
+puts "pick a past-tense verb:"
 past_verb = gets.chomp
+
+puts "Finally! Let's see how our story turned out! Dun Dun DUUUNNNNN"
+puts "Эта история основана на реальных событиях!"
+puts "Wait...what the f? Are the Russian hacking us again?!!! Let's try this again..."
+puts "Once upon a time there was a pretty little " + noun + " who, for some reason, decided to take a walk in the woods. like WTF, right?
+      Anyway, moving on. One day she ran into a(n) " + adj + ", and stinky, " + living_thing + " who " + adverb + " decided to sing a jingle then " + verb + " and gave her " + number + " shovels.
+      Next thing you know, this " + living_thing + " decided to " + verb_two + " into his stupid little treehouse where he hid his "
+      + noun_two + " and " + past_verb + " and ultimatley moved to outer space."
