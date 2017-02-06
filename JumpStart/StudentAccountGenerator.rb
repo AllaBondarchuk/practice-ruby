6ec705ee9f874b5bbc3c79d96448d17656f301b1
alla bondarchuk <allabnd@yahoo.com>
2017-02-05 17:40:20 -0800

    practice run two

--- JumpStart/StudentAccountGenerator.rb
+++ JumpStart/StudentAccountGenerator.rb
@@ -1,6 +1,7 @@
 # Practice skills: STRINGS, RANDOM NUMBERS, ARRAYS AND ITERATORS
-
 # START BY CREATING 3 ARRAYS
+Practice Run 1
+
 names = Array.new
 idnumbers = Array.new
 emails = Array.new
@@ -20,4 +21,27 @@ end
 
 5.times do |i|
   puts names [i] + "  " + idnumbers[i].to_s + "  " + emails[i]
+end
+
+Practice Run 2 (Random Ages)
+
+names = Array.new
+ages = Array.new
+emails = Array.new
+
+3.times do |i|
+  puts "Provide a name in this format: firstname lastname"
+  names << gets.chomp
+end
+
+3.times do |i| #can leave in or out, test it.
+  ages << rand(12..143)
+end
+
+3.times do |i|
+  emails << names[i].chr + names[i].split.last + ages[i].to_s + "@sexy.com"
+end
+
+3.times do |i|
+  puts names [i] + "  " + ages[i].to_s + "  " + emails[i]
 end
