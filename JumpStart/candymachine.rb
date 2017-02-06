8abfdf3e9b0f254cd68375e382e9b9b49fc57ba5
alla bondarchuk <allabnd@yahoo.com>
2017-02-05 18:32:42 -0800

    step break1

--- JumpStart/candymachine.rb
+++ JumpStart/candymachine.rb
@@ -7,10 +7,33 @@ response = gets.chomp.to_f
 # ".to_f" converts string to number (float)
 #example: user puts "1", .to_f converts to "1.00"
 
-puts "Hmm, $" + "%.2f" % response + " that's is it? Let's look at your options"
+puts "$" + "%.2f" % response + " that's is it? Let's look at your options"
+#"%.2f" dispalys the response
 
 puts "A $0.65 gummy worms"
 puts "B $0.50 skittles"
-puts "C $0.75 nutter butter"
+puts "C $0.75 licorice"
 puts "D $0.65 peanut butter cup"
 puts "E $0.55 cookies"
+
+puts "So?! Hurry up and choose something before Armageddon hits and you will starve!"
+
+choice = gets.chomp
+
+if choice.downcase == "a" || choice == "b" || choice == "c" || choice == "d" || choice == "e"
+	#write all possible choices
+	if choice.downcase == "a"
+		if response >= 0.65
+			print "Gummies 4 daaaayyzz. Take your change of $"
+			puts "%.2f" % (response-0.65)
+		else
+			puts "Sorry, the zombies will eat you now."
+		end
+
+	elsif choice.downcase == "b"
+		if response >= 0.50
+			print " Skittles to share! Take your change of $"
+			puts "%.2f" % (response-0.50)
+		else
+			puts "Sorry, should've saved more money while you could."
+		end
